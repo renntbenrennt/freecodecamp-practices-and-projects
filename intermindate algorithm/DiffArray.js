67fb7c2f75dc971511449ad0f8cd7394ac51effa
Benjamin <jijileeeeeee@gmail.com>
2019-10-19 21:56:29 +0800

    Create the repo for keeping up the old program, FCC
    I stopped the path FCC have laid for those who want to have a shot
    in Front end Developer role work when I got the job after
    just completed the first certificate...
    Now I want to continue the path and use the exercise in the program
    to practice my knowledge
    
    Right now, there are two of the JS certificate have been rewritten.
    
    I will update more in the following days.
    
    😎 Always be curious and keep learning! 😁

--- intermindate algorithm/DiffArray.js
+++ intermindate algorithm/DiffArray.js
@@ -1,3 +1,21 @@
+/**
+ * 
+ * Compare two arrays and return a new array 
+ * with any items only found in one of the two given arrays, 
+ * but not both. 
+ * In other words, return the symmetric difference of the two arrays. 
+ * 
+ * Note:
+ * You can return the array with its elements in any order.
+ * 
+ */
+
+
+/**
+ * 
+ * @param {Array} arrA 
+ * @param {Array} arrB 
+ */
 function diffArray(arrA, arrB) {
     let combinedArr = arrA.concat(arrB);
 
--- intermindate algorithm/SumAllNumbersInARange.js
+++ intermindate algorithm/SumAllNumbersInARange.js
@@ -1,3 +1,20 @@
+/**
+ * Question Description:
+ * 
+ * We'll pass you an array of two numbers. 
+ * Return the sum of those two numbers plus the sum of all the numbers between them. 
+ * The lowest number will not always come first.
+ * 
+ * For example, sumAll([4,1]) should return 10 
+ * because sum of all the numbers between 1 and 4 (both inclusive) is 10.
+ *  
+ */
+
+
+/**
+ * 
+ * @param {Array} arr 
+ */
 function sumAll(arr) {
     // The following solution is only limited in the case of
     // only two elements in the array
