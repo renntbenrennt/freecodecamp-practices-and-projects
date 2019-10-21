8b5d4b34f9478a026e4508977b3d0c68b8aba7d9
Benjamin <jijileeeeeee@gmail.com>
2019-10-21 19:33:26 +0800

    Finish Sorted Union question

---
+++ intermindate algorithm/SortedUnion.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * Write a function that takes two or more arrays and returns a new array of unique values 
+ * in the order of the original provided arrays. 
+ * 
+ * In other words, all values present from all arrays should be included in their original order, 
+ * but with no duplicates in the final array.
+ * 
+ * The unique numbers should be sorted by their original order, 
+ * but the final array should not be sorted in numerical order. 
+ * 
+ * Test Case:
+ * 
+ * uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+ * uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+ * uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
+ * 
+ */
+
+function uniteUnique(...arr) {
+    let setOfStuff = new Set();
+
+    arr.forEach(outterEle => {
+        outterEle.forEach(
+            innerEle => {
+                setOfStuff.add(innerEle);
+            }
+        );
+    });
+
+    return Array.from(setOfStuff);
+}
