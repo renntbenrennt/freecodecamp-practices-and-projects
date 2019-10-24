df657c7eae75b91c01b59950b357e648fa332283
Benjamin <jijileeeeeee@gmail.com>
2019-10-24 14:59:06 +0800

    add question des to ConvertHTMLEntities

--- intermindate algorithm/ConvertHTMLEntities.js
+++ intermindate algorithm/ConvertHTMLEntities.js
@@ -1,3 +1,19 @@
+/**
+ * 
+ * Convert the characters &, <, >, " (double quote), and ' (apostrophe), 
+ * in a string to their corresponding HTML entities.
+ * 
+ * Test Case:
+ * convertHTML("Dolce & Gabbana") should return Dolce &amp; Gabbana.
+ * convertHTML("Hamburgers < Pizza < Tacos") should return Hamburgers &lt; Pizza &lt; Tacos.
+ * convertHTML("Sixty > twelve") should return Sixty &gt; twelve.
+ * convertHTML('Stuff in "quotation marks"') should return Stuff in &quot;quotation marks&quot;.
+ * convertHTML("Schindler's List") should return Schindler&apos;s List.
+ * convertHTML("<>") should return &lt;&gt;.
+ * convertHTML("abc") should return abc.
+ *  
+ */
+
 function convertHTML(str) {
     let res = '';
 
