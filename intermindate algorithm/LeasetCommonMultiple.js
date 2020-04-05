2dfca6625119a67e632dcb69f253b0844cce31c8
Benjamin <jijileeeeeee@gmail.com>
2020-04-05 10:31:14 +0800

    finish Drop it

--- intermindate algorithm/LeasetCommonMultiple.js
+++ intermindate algorithm/LeasetCommonMultiple.js
@@ -39,6 +39,28 @@ function LCM(a, b) {
     return Math.abs(a * b) / GCD(a, b);
 }
 
+/**
+ * 
+ * Find the smallest common multiple of the provided parameters 
+ * that can be evenly divided by both, 
+ * as well as by all sequential numbers in the range between these parameters. 
+ * The range will be an array of two numbers 
+ * that will not necessarily be in numerical order. 
+ * 
+ * For example, if given 1 and 3, 
+ * find the smallest common multiple of both 1 and 3 
+ * that is also evenly divisible by all numbers between 1 and 3. 
+ * The answer here would be 6.
+ * 
+ * smallestCommons([1, 5]) should return a number.
+ * smallestCommons([1, 5]) should return 60.
+ * smallestCommons([5, 1]) should return 60.
+ * smallestCommons([2, 10]) should return 2520.
+ * smallestCommons([1, 13]) should return 360360.
+ * smallestCommons([23, 18]) should return 6056820.
+ * 
+ * @param {*} arr 
+ */
 function lcmOfNums(arr) {
     /// initial attempt, fail at [23, 18] as final test...
     // let nums = [];
